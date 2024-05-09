600c3f217c36713063ca5bfd3d14a8a76f020fab
WandiaG <wandiagithinji@gmail.com>
2024-05-09 07:31:13 -0700

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Calculates the fewest number
+of operations needed to result in exactly n H
+characters in the file."""
+
+
+def minOperations(n: int) -> int:
+    """calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file"""
+    process = 2
+    op = 0
+    while n > 1:
+        while n % process == 0:
+            op += process
+            n /= process
+        process += 1
+    return op
